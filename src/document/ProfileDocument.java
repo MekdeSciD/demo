b2d8cac9ed532cf64c79bcb588d0fc78680158d5
MekdesK <48019991+MekdeSciD@users.noreply.github.com>
2020-01-06 15:25:03 -0500

    profile setup

--- src/document/ProfileDocument.java
+++ src/document/ProfileDocument.java
@@ -1,4 +1,10 @@
 package document;
 
 public class ProfileDocument {
+    private String id;
+    private String firstName;
+    private String lastName;
+    private List<Technologies> technologies;
+    private List<String> emails;
+
 }
